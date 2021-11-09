643db75997c40ddf7d7b9f0954f8c74402c47781
IsraEB <sr.enriquez@yahoo.com>
2021-11-09 10:57:33 -0600

    bat

---
+++ FaseA.bat
@@ -0,0 +1,26 @@
+@Echo Off
+CLS
+
+del %1.FT2 2> nul
+del %1.FT1 2> nul
+
+java AnalizadorLexicografico.java %1
+
+IF errorlevel 1 GOTO FALLLO
+
+echo Analizador Lexicografico Terminado
+
+java ParserSLR1.java %1
+
+IF errorlevel 1 GOTO FALLLO
+
+echo Analisis sintactico SLR(1) Terminado
+GOTO FIN
+
+:FALLLO
+ECHO ERRORES EN LLA COMPILACION!!!
+
+:FIN
+ECHO COMPILLACION TERMINADA
+
+:SALLIR
